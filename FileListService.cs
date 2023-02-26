a178b1bd12317923a84786406518ae02816fee00
AjeetKumar <xlquark@gmail.com>
2023-02-26 10:11:12 +0530

    Simple Windows Service. To list files contained inside 'Files' folder; the folder located at Desktop. The service can run with and without System.Timers.Timer

--- FileListService.cs
+++ FileListService.cs
@@ -4,6 +4,15 @@ using System.Timers;
 
 namespace FileListService
 {
+    /// <summary>
+    /// Uncomment the code commented to run the service after lapse of 60000 milliseconds
+    /// timer.Interval = 60000; implies that when timer ticks after 60000 milliseconds, event fires
+    /// which fires the event handler delegate method OnElaspedListFiles
+    /// timer.AutoReset=false; will run the service only once after lapse of 60000 milliseconds but
+    /// service will take as much time as needed to complete the task 'OnElaspedListFiles'
+    /// timer.AutoReset=true; will run the service again after lapse of 60000 milliseconds
+    /// timer.Enabled=true allows timer to tick.
+    /// </summary>
     public partial class FileListService : ServiceBase
     {
         System.Timers.Timer timer;
--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ namespace FileListService
     {
         /// <summary>
         /// The main entry point for the application.
+        /// The Main method is executed first as entry point of the program
+        /// The windows service is a class which derives all the properties and methods of
+        /// ServiceBase class. Here we use array of ServiceBase variable named as ServicesToRun
+        /// and initializes the ServicesToRun. The static method Run of ServiceBase class runs 
+        /// all the service objects used to initialize the ServicesToRun variable.
+        /// We could have just created a ServiceBase variable 'ServiceToRun' and initialized it 
+        /// with ServicesToRun= new FileListService(); Then ServiceBase.Run(ServiceToRun);
+        /// This will also work.
         /// </summary>
         static void Main()
         {
