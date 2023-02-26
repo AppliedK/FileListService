e99b151a86bb3cff04ae0437da80c7473255f6bf
AjeetKumar <xlquark@gmail.com>
2023-02-26 09:53:36 +0530

    Add project files.

---
+++ FileListService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace FileListService
+{
+    public partial class FileListService : ServiceBase
+    {
+        System.Timers.Timer timer;
+        FilesList filesList;
+        public FileListService()
+        {
+            InitializeComponent();
+            //timer = new System.Timers.Timer();
+            //timer.Enabled = true;
+            //timer.Interval = 60000;
+            //timer.AutoReset = false;
+        }
+
+        private void OnElaspedListFiles(object sender, ElapsedEventArgs e)
+        {
+            filesList = new FilesList();
+            filesList.ListFiles();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            filesList = new FilesList();
+            filesList.ListFiles();
+            filesList.WriteLog("Service started at " + DateTime.Now.ToString());
+            //timer.Elapsed += new System.Timers.ElapsedEventHandler(OnElaspedListFiles);
+        }
+
+        protected override void OnStop()
+        {
+            //timer.Enabled = false;
+            filesList.WriteLog("Service stopped at " + DateTime.Now.ToString());
+        }
+    }
+}
---
+++ FilesList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileListService
+{
+    internal class FilesList
+    {
+        private readonly string desktop;
+        public FilesList()
+        {
+            desktop = @"C:\Users\dell\Desktop";
+        }
+        public void ListFiles()
+        {
+            var folderPath = Path.Combine(desktop, "Files");
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (string file in files)
+            {
+                WriteLog(file + " read at " + DateTime.Now.ToString());
+            }
+        }
+        public void WriteLog(string message)
+        {
+            string filepath = Path.Combine(desktop, "list.txt");
+            using (StreamWriter sw = new StreamWriter(filepath, true))
+            {
+                if (File.Exists(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
